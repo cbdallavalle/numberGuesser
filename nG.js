11b62aec20b62e9fc40eec91d378970b71911c41
Casey Dallavalle <cbdallavalle@gmail.com>
2017-10-10 20:26:33 -0600

    Adding phase 3 complete

---
+++ nG.js
@@ -0,0 +1,329 @@
+
+//Default min and max
+var min =  0;
+var max = 100;
+
+//Default chooses random number
+function chooseRandom() {
+  var chooseNum = Math.floor((Math.random() * max - min) + 1);
+  console.log(chooseNum);
+  return chooseNum
+}
+
+var randomNum = chooseRandom();
+
+//Reset the random number when a new minimum and maximum are given
+var inputForm = document.getElementById('input-form');
+var enterButton = document.querySelector('.enter-min-max');
+
+enterButton.addEventListener('click', function() {
+  enter();
+  findNewRandom();
+});
+
+function findNewRandom() {
+  randomNum = chooseRandom();
+}
+
+//Enter own min and max
+function enter() {
+  event.preventDefault();
+  min = parseInt(document.getElementById('enter-min').value);
+  max = parseInt(document.getElementById('enter-max').value);
+  displayMin.innerText = min;
+  displayMax.innerText = max;
+  // console.log(min, max);
+}
+
+//Variables target the numerical values between Guess the number between 0 and 100
+var displayMin = document.querySelector('.min-value');
+var displayMax = document.querySelector('.max-value');
+
+//clear input field on click
+var clearButton = document.querySelector('.clear-button');
+clearButton.addEventListener('click', clear);
+
+function clear() {
+  // console.log('submit clear function working');
+   input.value = '';
+   ul.innerText = '0';
+   disableClear();
+ }
+
+//Disable clear button when nothing to clear
+var input = document.getElementById('enter-guess');
+input.addEventListener('keyup', disableClear);
+
+  function disableClear() {
+    // console.log(input.value)
+  if (input.value === '') {
+    clearButton.disabled = true;
+    // console.log('if disabled is running');
+  }
+
+  else {
+   clearButton.disabled = false;
+   // console.log('else disabled is running')
+  }
+}
+
+
+//reset button will reload the page, effectively re-running the randomNumber function and returning display to 0
+var resetButton = document.querySelector('.reset-button');
+resetButton.addEventListener('click', reset);
+
+function reset() {
+  window.location.reload(true);
+  disableReset();
+  ul.innerText= "0";
+}
+
+//Disable reset button upon load
+input.addEventListener('keyup', disableReset);
+
+  function disableReset() {
+  if (input.value === '') {
+    resetButton.disabled = true;
+    // console.log('if disabled is running');
+  }
+
+  else {
+   resetButton.disabled = false;
+   // console.log('else disabled is running')
+  }
+}
+
+//When Guess button is pressed, takes the input value and compares it to the chosen random number.
+var ul = document.querySelector('ul');
+var guessButton = document.querySelector('.guess-button');
+
+var hints = document.getElementById('hints');
+var lastGuessStr = document.querySelector('.last-guess-str');
+
+guessButton.addEventListener('click', function(e){ 
+  e.preventDefault();
+  console.log("I am checking to see if enter button is working", e);
+  submit();
+});
+
+ function submit() {
+   console.log('submit function running');
+   var numString = input.value;
+   ul.innerText = numString;
+   
+    var userNum = parseInt(numString);
+    console.log(userNum);
+
+    //Checks to see if user number is between min and max, and whether it is NaN
+    if (isNaN(parseInt(numString))) {
+        lastGuessStr.innerText = 'Don\'t mess with me.';
+        ul.innerText = '(-_-)';
+        hints.innerText = 'I know that\'s not a number!';
+       return;
+      }
+
+    else if (userNum > max) {
+      lastGuessStr.innerText = 'Your guess is too big';
+      ul.innerText = '0';
+      hints.innerText = '';
+      return;
+    }
+    else if (userNum < min) {
+      lastGuessStr.innerText = 'Your guess is too small';
+      ul.innerText = '0';
+      hints.innerText = '';
+      return;
+    }
+
+    //If number is between min and max, displays hints
+    else {
+      
+      if (userNum === randomNum) {
+        lastGuessStr.innerText = 'Your last guess was';
+        hints.innerText = 'BOOM!';
+        uponSuccess();
+         }
+      else if (userNum > randomNum) {
+        lastGuessStr.innerText = 'Your last guess was';
+        hints.innerText = 'That is too high';
+          }
+      else {
+        lastGuessStr.innerText = 'Your last guess was';
+        hints.innerText = 'That is too low';
+      }
+    }
+  }
+
+function display0() {
+  var ulDisplay = ul.innerText;
+  var ulDisplay0 = 0;
+}
+
+function uponSuccess () {
+  min = min - 10;
+  max = max + 10;
+  randomNum = chooseRandom();
+  displayMin.innerText = min;
+  displayMax.innerText = max;
+  lastGuessStr.innerText = 'Try again! It\' harder this time, though...';
+  setTimeout(function() 
+    { ul.innerText = 0;
+      hints.innerText = '';
+    }, 1500);
+    }
+
+//incrementing min and max upon successful guess
+//write a function, that recognizes when hints = Boom!
+//calls min and max and increments their values by ten
+//calls chooseRandom to pick a new random number in between new min and max
+//displays new min and max in top paragraph
+
+
+// // testing enter own min and max
+// var enterButton = document.querySelector('.enter-min-max');
+// enterButton.addEventListener('click', enter);
+
+// function enter() {
+//   var enterMin = document.getElementById('enter-min').value;
+//   var enterMax = document.getElementById('enter-max').value;
+//   var min = enterMin;
+//   var max = enterMax;
+//   console.log(min);
+//   console.log(max);
+// }
+
+
+// //Chooses random number
+// function chooseRandom() {
+//   var chooseNum = Math.floor((Math.random() * 100) + 1);
+//   return chooseNum
+// }
+
+// var randomNum = chooseRandom();
+
+// //various variables defined
+// var ul = document.querySelector('ul');
+// var guessButton = document.querySelector('.guess-button');
+// guessButton.addEventListener('click', submit);
+// var min = 0;
+// var max = 100;
+
+
+//  function submit() {
+//    console.log('submit function running');
+//    var numString = document.getElementById('enter-guess').value;
+//    ul.innerText = numString;
+   
+//     var userNum = parseInt(numString);
+//     console.log(userNum);
+
+//     //Checks to see if user number is between min and max, and whether it is NaN
+//     if (isNaN(parseInt(numString))) {
+//         alert('Don\'t mess with me')
+//         ul.innerText = '0';
+//         return;
+//       }
+
+//     else if (userNum > max) {
+//       alert('Your number is too big');
+//       ul.innerText = '0';
+//       return;
+//     }
+//     else if (userNum < min) {
+//       alert('Your number is too small')
+//       ul.innerText = '0';
+//       return;
+//     }
+
+//     //If number is between min and max, displays hints
+//     else {
+//       var hints = document.getElementById('hints');
+      
+//       if (userNum === randomNum) {
+//         hints.innerText = 'BOOM!';
+//          }
+//       else if (userNum > randomNum) {
+//         hints.innerText = 'That is too high';
+//           }
+//       else {
+//         hints.innerText = 'That is too low';
+//       }
+//     }
+//   }
+
+// //clear button will clear input and return display to 0
+// var clearButton = document.querySelector('.clear-button');
+// clearButton.addEventListener('click', clear);
+
+// function clear() {
+//   console.log('submit clear function working');
+//    document.getElementById('enter-guess').value = 'Enter your guess';
+//    ul.innerText = '0';
+//  }
+
+//testing
+// var resetButton = document.querySelector('.reset-button');
+// resetButton.addEventListener('click', reset);
+
+// function reset() {
+//   'nG.html'.reload(true);
+
+
+
+
+
+
+//testing
+// var resetButton = document.querySelector('.reset-button');
+// resetButton.addEventListener('click', reset);
+
+// function reset() {
+//   "nG.html".reload(true);
+
+// var clearButton = document.querySelector('.clear-button');
+// clearButton.addEventListener('click', clear);
+
+// function clear() {
+//   console.log('submit clear function working');
+//    ul.innerText = "Enter your guess";
+
+
+
+// var userString = document.getElementById("enter-guess").textContent;
+// //Do I need a function here?
+// var userNum = parseInt(userString);
+// userString.textcontent = userNum
+
+
+
+// //Choosing a random number and comparing it to the user number//
+// function chooseRandom() {
+//   var chooseNum = Math.floor((Math.random() * 100) + 1);
+//   return chooseNum
+// }
+
+// var randomNum = chooseRandom();
+
+// //?how to get these to display in <p> class="hints"//
+// function numComparison() {
+//   if (userNum === randomNum) {
+//     return "BOOM!"
+//   }
+//   else if (userNum > randomNum) {
+//     return "That is too high"
+//   }
+//   else 
+//     return "That is too low"
+// }
+
+// //When the guess button is clicked, will display the number in big pink text//
+// var ul = document.querySelector('ul');
+// var guessButton = document.querySelector('.guess-button');
+// guessButton.addEventListener('click', submit);
+
+
+//  function submit() {
+//    var numGuess = document.getElementById('enter-guess').value;
+//    ul.innerText = numGuess;
+//    }
+
